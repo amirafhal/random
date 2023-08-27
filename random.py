ed1fe7f8bce987cd3314e7f92043e6f0860622b6
Amira Fh <87784799+amirafhal@users.noreply.github.com>
2023-08-27 21:15:27 +0100

    Update random.py

--- random.py
+++ random.py
@@ -1,13 +1,13 @@
 import random
 value = random.randint(10,20)
-c = 5
-while c > 0:
+c = 0
+while c < 5:
   user = int(input("enter your value: "))
   if user == value:
     print(f"You win after {c} chances and the value is {value}")
     break
   else:
-    c-=1
+    c+=1
     if user > value:
       print("The value random is less than your value")
     else:
